7404d436cb083c11ca2a9f81597665d2809370b4
Lim Howe <limhowe1214@gmail.com>
2016-11-29 10:18:48 +0800

    Feat: added shared button group

---
+++ src/components/ButtonGroup/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Button } from 'react-toolbox';
+
+import styles from './ButtonGroup.scss';
+
+export default class ButtonGroup extends Component {
+  displayName: 'ButtonGroup'
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.value && nextProps.value !== this.props.value) {
+      this.setState({ value: nextProps.value });
+    }
+  }
+
+  props: {
+    source: Array<Object>,
+    onChange: Function,
+    value: string
+  }
+
+  onChange = (value) => {
+    this.props.onChange(value);
+  }
+
+  renderButtons() {
+    return this.props.source.map((props) => {
+      const { label, icon, value } = props;
+      return (
+        <Button
+          theme={ styles }
+          icon={ icon }
+          label={ label }
+          key={ value }
+          onClick={ () => this.onChange(value) }
+          raised={ this.state.value !== value }
+        />
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        { this.renderButtons() }
+      </div>
+    );
+  }
+}
