c17314ca1b4c750a7f244ddc6ef0cf9882826fa6
Lim Howe <limhowe1214@gmail.com>
2016-12-03 00:05:47 +0800

    Fix: removed old code

--- src/modules/App/Campaigns/index.js
+++ src/modules/App/Campaigns/index.js
@@ -1,5 +1,4 @@
 export CampaignsList from './CampaignsList';
-export CampaignStart from './CampaignStart';
 export CampaignEdit from './CampaignEdit';
 
 export default from './CampaignsList';
--- src/modules/App/index.js
+++ src/modules/App/index.js
@@ -1,6 +1,6 @@
 export Login from './Login';
 export Dashboard from './Dashboard';
-export { CampaignsList, CampaignStart, CampaignEdit } from './Campaigns';
+export { CampaignsList, CampaignEdit } from './Campaigns';
 export { ApplicationList, ApplicationEdit } from './Applications';
 export { DeviceAnalytics, UserAnalytics, EventAnalytics } from './Analytics';
 export { SegmentList, SegmentEdit } from './Segments';
